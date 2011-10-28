035302f05cbd5ea01a5f50b12a4614d7960621da
jitu keshri <jitukeshri@jitus-Mac-mini.local>
2011-10-28 14:50:17 +0530

    Initial Commit

---
+++ Demo2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo2
+//
+//  Created by jitu keshri on 10/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo2
+//
+//  Created by jitu keshri on 10/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
