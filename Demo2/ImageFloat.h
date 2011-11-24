8a743b281d7324d05878710d418c6e5e5ec96a45
Shashi Podeti <shashi.k.podeti@gmail.com>
2011-11-24 16:52:40 +0530

    semi working image floats

---
+++ Demo2/ImageFloat.h
@@ -0,0 +1,27 @@
+//
+//  ImageFloat.h
+//  Demo2
+//
+//  Created by jitu keshri on 11/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageFloat : UIViewController 
+<UIImagePickerControllerDelegate , UINavigationControllerDelegate>
+
+{
+    
+    
+    
+    UIImagePickerController *picker;
+    
+    IBOutlet UIImageView * selectedImage;
+    
+}   
+
+@property (nonatomic, retain) UIImageView * selectedImage;
+
+- (IBAction) buttonClicked;
+@end
